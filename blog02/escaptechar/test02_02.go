328d754499a814208579daf7c6f5230f8c616d7e
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-01-29 00:05:45 +0800

    Add files via upload

---
+++ blog02/escaptechar/test02_02.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt" //引入包fmt 提供格式化、输出、输入函数
+func main() {
+	//行注释：转义字符
+	fmt.Println("Eastmount\tCSDN\tyangxiuzhang")
+
+	/*
+	 块注释
+	 Go语言常用的注释方法
+	 注释提高了代码的阅读性
+	*/
+	fmt.Println("Hello\nWorld!!!")
+}

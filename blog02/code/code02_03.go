36eb0329a07395f2a0e8f3d69a525c4f13e6819d
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-01-29 17:49:42 +0800

    Add files via upload

---
+++ blog02/code/code02_03.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"    //引入包fmt 提供格式化、输出、输入函数
+func main() {
+	var a = 2
+	var b = 3
+	fmt.Println("a + b = ", a + b)
+	fmt.Println("a - b = ", a - b)
+	fmt.Println("a * b = ", a * b)
+	fmt.Println("a / b = ", a / b)
+}

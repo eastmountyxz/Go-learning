89f19adbe0f4184a454cf33287848a838d424c0e
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-01-30 22:28:06 +0800

    Add files via upload

---
+++ blog03/datatype/type03_02.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main() {
+	var i int = -20
+	fmt.Println("i =", i)
+
+	var j uint = 127
+	fmt.Println("j =", j)
+
+	var k rune = 1024
+	fmt.Println("k =", k)
+
+	var m byte = 255
+	fmt.Println("m =", m)
+}

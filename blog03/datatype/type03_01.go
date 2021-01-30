58050ea187e7537e57f85fb2c65eabd646c1dc73
eastmountyxz <53172375+eastmountyxz@users.noreply.github.com>
2021-01-30 22:27:30 +0800

    Create  type03_01.go

---
+++ blog03/datatype/type03_01.go
@@ -0,0 +1,16 @@
+package main
+import "fmt"
+
+func main() {
+	var i int = 1
+	fmt.Println("i =", i)
+
+	var j int8 = 127
+	fmt.Println("j =", j)
+
+	var k uint16 = 255
+	fmt.Println("k =", k)
+
+	var m uint8 = 256
+	fmt.Println("m =", m)
+}
